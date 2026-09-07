3574093dce3a18ca4bdb28d85bbe1a60473c03f0
shrX <shrikar.zip@gmail.com>
2026-09-08 02:09:06 +0530

    Revert "basic UI"
    This reverts commit 66d868bd12da2728e7fe7c294f1553a55f2e4242.

--- frontend/smriti_app/README.md
+++
@@ -1,3 +0,0 @@
-# smriti_app
-
-A new Flutter project.
--- frontend/smriti_app/android/app/src/main/kotlin/com/example/smriti_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.smriti_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
